09a1bc2e61e843d08dedc0278f3347d881e41f72
Ramaguru Radhakrishnan <7790256+ramagururadhakrishnan@users.noreply.github.com>
2022-12-08 18:27:35 +0530

    Add files via upload

--- Omtool/OmCLI.py
+++ Omtool/OmCLI.py
@@ -13,21 +13,23 @@ Tool Developed by Team ModSouls, TIFAC-CORE in Cyber Security, Amrita Vishwa Vid
 '''
 
 import os
-import subprocess
 import time
 import sys
+from colorama import Fore
 import socket
 import requests
 import argparse
+import schedule
+import time
 import utils.twitter_scraping
-from utils import (search_realname, search_username, ig_scrape, whois_lookup, webhook_spammer, 
-ip_scanner, ip_lookup, phonenumber_lookup, websearch, smsbomber, tokenlogger_generator, 
-twitter_scraping)
-from colorama import Fore
+from utils import search_realname, search_username, ig_scrape, whois_lookup, webhook_spammer, ip_scanner, ip_lookup, \
+    phonenumber_lookup, websearch, smsbomber, tokenlogger_generator, twitter_scraping
+from subtools import omuser_search, omupi_search
+
 
 if os.name == "nt":
     os.system("cls")
-    os.system("title H4X-Tools")
+    os.system("title Om Tool")
 if os.name == "posix":
     os.system("clear")
 
@@ -50,89 +52,97 @@ def main():
     print(Fore.CYAN + """
 [+]    
 |
-|  ███╗   ███╗   ██████╗   ██████╗   ███████╗   ██████╗   ██╗   ██╗  ██╗       ███████╗ 
-|  ████╗ ████║  ██╔═══██╗  ██╔══██╗  ██╔════╝  ██╔═══██╗  ██║   ██║  ██║       ██╔════╝
-|  ██╔████╔██║  ██║   ██║  ██║  ██║  ███████╗  ██║   ██║  ██║   ██║  ██║       ███████╗
-|  ██║╚██╔╝██║  ██║   ██║  ██║  ██║  ╚════██║  ██║   ██║  ██║   ██║  ██║       ╚════██║     
-|  ██║ ╚═╝ ██║  ╚██████╔╝  ██████╔╝  ███████║  ╚██████╔╝  ╚██████╔╝  ███████╗  ███████║  
-|  ╚═╝     ╚═╝   ╚═════╝   ╚═════╝   ╚══════╝   ╚═════╝    ╚═════╝   ╚══════╝  ╚══════╝
+|   ██████╗  ███╗   ███╗      ███╗   ███╗  ██████╗   ██████╗   ███████╗   ██████╗   ██╗   ██╗  ██╗       ███████╗ 
+|  ██╔═══██╗ ████╗ ████║      ████╗ ████║ ██╔═══██╗  ██╔══██╗  ██╔════╝  ██╔═══██╗  ██║   ██║  ██║       ██╔════╝
+|  ██║   ██║ ██╔████╔██║      ██╔████╔██║ ██║   ██║  ██║  ██║  ███████╗  ██║   ██║  ██║   ██║  ██║       ███████╗
+|  ██║   ██║ ██║╚██╔╝██║  ██  ██║╚██╔╝██║ ██║   ██║  ██║  ██║  ╚════██║  ██║   ██║  ██║   ██║  ██║       ╚════██║     
+|  ╚██████╔╝ ██║ ╚═╝ ██║      ██║ ╚═╝ ██║ ╚██████╔╝  ██████╔╝  ███████║  ╚██████╔╝  ╚██████╔╝  ███████╗  ███████║  
+|   ╚═════╝  ╚═╝     ╚═╝      ╚═╝     ╚═╝  ╚═════╝   ╚═════╝   ╚══════╝   ╚═════╝    ╚═════╝   ╚══════╝  ╚══════╝
 | by M0ds0uls (https://github.com/m0d4s0uls)
 |
 |
 |
 [+]
     """)
-    internet_check()
+    internet_check()  
 
     while 1:
         print(Fore.CYAN)
-        print("[1] Username Search + Web search")
-        print("[2] Number search")
-        print("[3] Email search")
-        print("[4] About")
+        print("[1] Sheduled Search")
+        print("[2] Periodical Search")
+        print("[3] Recorded Search")
+        print("[4] Live Search")
         print("\n")
-
-        a = input("[+] Select your option : \t")
-
-        if a=="1":
-            query = str(input("Search query : \t"))
-            websearch.Search(query)
-            username = query.replace(" ", "_")
+        b = input("[+] Select your option:\t")
+        if b=="1":
+            print(Fore.CYAN)
+            print("[a] Username Search + Web Search")
+            print("[b] Number Search")
+            print("[c] Email Search")
+            print("[d] UPI Search")
+            print("[e] Image Search")
+            print("[f] About")
             print("\n")
-            search_realname.facebook(username)
-            search_realname.linkedin(username)
-            search_realname.whitepages(username)
-            search_realname.peoplefinders(username)
+            a = input("[+] Select your option : \t")
+            b= input("[+] Select query to search:\t")
+            c=input("[+] select the day to search:\t")
             print("\n")
-            search_username.instagram(username)
-            search_username.pinterest(username)
-            search_username.twitter(username)
-            search_username.youtube(username)
-            search_username.github(username)
-            search_username.stackoverflow(username)
-            search_username.steam(username)
-            search_username.reddit(username)
-            # search_username.tiktok(username)
-            # search_username.twitch(username)
+        if b=="2":
+            print(Fore.CYAN)
+            print("[a] Username Search + Web Search")
+            print("[b] Number Search")
+            print("[c] Email Search")
+            print("[d] UPI Search")
+            print("[e] Image Search")
+            print("[f] About")
+            print("\n")
+            a = input("[+] Select your option : \t")
+            b= input("[+] Select query to search:\t")
+            c=input("[+] select the frequency time to search:\t")
+            print("\n")
+        if b=="3":
+            print(Fore.CYAN)
+            print("[a] Username Search + Web Search")
+            print("[b] Number Search")
+            print("[c] Email Search")
+            print("[d] UPI Search")
+            print("[e] Image Search")
+            print("[f] About")
+            print("\n")
+            a = input("[+] Select your option : \t")
+            b= input("[+] Select query to search:\t")
+            print("\n")
+            print("Taking the input from DB....")
+            print("\n")
+        if b=="4":
+            while 1:
+                print(Fore.CYAN)
+                print("[a] Username Search + Web Search")
+                print("[b] Number Search")
+                print("[c] Email Search")
+                print("[d] UPI Search")
+                print("[e] Image Search")
+                print("[f] About")
+                print("\n")
 
-        elif a=="2":
-            no = str(input("Enter a phone-number without country code : \t"))
-            phonenumber_lookup.Number("+91"+no) 
-            target = no
-            main_url = 'https://upibankvalidator.com/api/upiValidation?upi='
-            upi_ids = ['apl','oksbi','okhdfcbank','sbi','okicici','axisbank','ikwik','pnb','hdfc','idfcbank','okaxis','abfspay','ybl','barodapay','upi','paytm','yapl']
+                a = input("[+] Select your option : \t")
 
-            if len(target) > 9:
+                if a=="a":
+                    omuser_search.omuser_search();
 
-                if target.find('@')!=-1:
-                    response = requests.post(main_url+target).json()
-
-                    if response['isUpiRegistered']:
-                # print('found')
-                        # print(Fore.CYAN)
-                        print(f"{Fore.BLUE}✅ UPI Holder Name: {response['name']}",f"| UPI platform: {target.partition('@')[2]}")
-                    else:
-                        # print(Fore.RED)
-                        # print('Invalid UPI Not found in',i)
-                         print(f"{Fore.WHITE}❌ Invalid UPI Not found in",i)
+                elif a=="b":
+                    no = str(input("Enter a phone-number: \t"))
+                    phonenumber_lookup.Number("+91 " + no) 
+                    omupi_search.omupi_search(no);
+                elif a=="c":
+                    email= input("Enter email to search without @XXX.com:\t")
+                    omupi_search.omupi_search(email);
+                elif a=="d":
+                    upi=input("Enter UPI or number without @xxx:\t")
+                    omupi_search.omupi_search(upi);
                 else:
-                    for i in upi_ids:
-                            response = requests.post(main_url+target+'@'+i).json()
-                            if response['isUpiRegistered']:
-                    # print('found')
-                                print(f"{Fore.GREEN}✅ UPI Holder Name: {Fore.RED}{response['name']}",f"| UPI platform: {i}")
-                            else:
-                                # print(Fore.RED)
-                                # print('Invalid UPI Not found in',i)  
-                                print(f"{Fore.WHITE}❌ Invalid UPI Not found in",i)
-
-        # elif a=="3":
-
-            
-
-        else:
-            print(Fore.RED + "Invalid option!")
-            time.sleep(1)
+                    print(Fore.RED + "Invalid option!")
+                    time.sleep(1)
 
 if __name__ == '__main__':
     main()      
---
+++ Omtool/subtools/omuser_search.py
@@ -0,0 +1,34 @@
+import os
+import time
+import sys
+from colorama import Fore
+import socket
+import requests
+import argparse
+import schedule
+import time
+import utils.twitter_scraping
+from utils import search_realname, search_username, ig_scrape, whois_lookup, webhook_spammer, ip_scanner, ip_lookup, \
+    phonenumber_lookup, websearch, smsbomber, tokenlogger_generator, twitter_scraping
+from subtools import omuser_search
+
+def omuser_search():
+    query = str(input("Search query : \t"))
+    websearch.Search(query)
+    username = query.replace(" ", "_")
+    print("\n")
+    search_realname.facebook(username)
+    search_realname.linkedin(username)
+    search_realname.whitepages(username)
+    search_realname.peoplefinders(username)
+    print("\n")
+    search_username.instagram(username)
+    search_username.pinterest(username)
+    search_username.twitter(username)
+    search_username.youtube(username)
+    search_username.github(username)
+    search_username.stackoverflow(username)
+    search_username.steam(username)
+    search_username.reddit(username)
+    # search_username.tiktok(username)
+    # search_username.twitch(username)
